d9a8e675f20fc248b6d7308109009ee1064b6269
CamdenStubbs <camdenstubbs@byui.edu>
2025-01-11 18:50:44 -0700

    Mission Statement Created

---
+++ running/something.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("my-apple-element").textContent = myApples;
+document.getElementById("friend-apple-element").textContent = friendApples;
+document.getElementById("total-apple-element").textContent = total;
